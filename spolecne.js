9fd431c713cbc53a657dbcc35fdafaa380629c18
Martina Bujasová <martina.bujasova2@gmail.com>
2024-04-11 18:19:26 +0200

    bonus pridani tlacitek uloha 3

--- spolecne.js
+++ spolecne.js
@@ -2,6 +2,14 @@ const mojeFunkce = () => {
   const pridaniTridy = document.querySelector('#menu-polozky');
   pridaniTridy.classList.toggle('show');
   const zmenaTlacitka = document.querySelector('#menu-tlacitko .fa-bars');
+  if (zmenaTlacitka) {
+    zmenaTlacitka.classList.toggle('fa-bars');
+    zmenaTlacitka.classList.toggle('fa-xmark');
+  } else {
+    const zmenaTlacitka2 = document.querySelector('#menu-tlacitko .fa-xmark');
+    zmenaTlacitka2.classList.toggle('fa-bars');
+    zmenaTlacitka2.classList.toggle('fa-xmark');
+  }
 };
 
 const menuTlacitko = document.querySelector('#menu-tlacitko');
